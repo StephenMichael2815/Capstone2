8fa83616bfa75a800d4102108d72be5b57d37fa9
Stephen Michael <smichael2815@Stephens-MacBook-Pro.local>
2024-11-13 16:04:54 -0700

    update server

--- server/index.js
+++ server/index.js
@@ -3,18 +3,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const { client } = require("./db");
-
 client.connect();
-
+// Use CORS middleware
+app.use(cors());
 app.use(express.json());
-
 app.use("/api", require("./api"));
-
 app.use((err, req, res, next) => {
   console.log(err);
   res
     .status(err.status || 500)
     .send({ error: err.message ? err.message : err });
 });
-
 app.listen(port, () => console.log(`listening on port ${port}`));
