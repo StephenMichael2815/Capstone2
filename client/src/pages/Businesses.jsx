19a10c93a26325f28ab28b742380557baaafbf39
Stephen Michael <smichael2815@Stephens-MacBook-Pro.local>
2024-11-13 15:52:04 -0700

    added business

--- client/src/pages/Businesses.jsx
+++ client/src/pages/Businesses.jsx
@@ -1,8 +1,26 @@
-const Businesses = ({ businesses })=> {
+import React from "react";
+import { Button } from "@mui/material";
+
+const Business = ({ business, onReview }) => {
   return (
-    <h1>Placeholder for Businesses { businesses.length }</h1>
+    <div className="business">
+      <h2>{business.name}</h2>
+      <p>{business.description}</p>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "black",
+          color: "#FFFFFF",
+          "&:hover": {
+            backgroundColor: "#ff00cc",
+          },
+        }}
+        onClick={() => onReview(business.id)}
+      >
+        Review
+      </Button>
+    </div>
   );
-}
+};
 
-
-export default Businesses;
+export default Business;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 const serverPort = process.env.PORT || 3000;
-console.log(`api need to run on ${serverPort} for vite server`);
-// https://vitejs.dev/config/
+console.log(
+  `API needs to run on http://localhost:${serverPort} for Vite server`
+);
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `http://localhost:${ serverPort || 3000}`
-    }
-  }
-})
+      "/api": `http://localhost:${serverPort}`,
+    },
+  },
+});
